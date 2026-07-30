b64e89bce8ef80ca1c1d9cbb25164681ec273d87
Ayush <ayushkumar18102002@gmail.com>
2026-07-30 12:48:41 +0530

    file handling complete

--- file handling/file_handling.py
+++ file handling/file_handling.py
@@ -58,3 +58,75 @@ with open("demofile.txt") as f:
     for x in f:
         print(x) # it will print the whole file line by line
 
+
+
+
+# write to an existing file
+# to write a existing file you have to write a parameter to the open() function
+
+# "a" - append mode which is used to add new data to the end of the file
+# "w" - write mode which is used to edit and write on the file
+
+
+# open the file "demofile.txt" and append content to the file
+
+with open("demofile.txt", "a") as f:
+    f.write("now the file has more content")
+
+
+# open and red the file after appending 
+
+with open("demofile.txt", "r") as f:
+    print(f.read())
+
+
+# overwriting the existing content 
+
+with open("demofile.txt", "w") as f: # the " w " method will overwrite the entire file
+    f.write("woops i deletaed the content because of chiku chamcham masti")
+
+
+with open("demofile.txt","r") as f :
+    print(f.read())
+
+
+# create a new file 
+# to create a new file in pyhton use open() method with one parameter
+
+# "x" - create - will create and return error if file exists
+
+
+# create a new file 
+f = open("myfile.txt", "x")
+
+
+with open("myfile.txt","a") as f:
+    f.write("i create d a new file watch it for me ")
+
+with open("myfile.txt","r") as f:
+    print(f.read())
+
+
+# python delete file 
+ # through the os.remove() you can delete a file 
+
+ import os 
+ os.remove("demofile.txt")
+
+
+# check if file exists 
+
+import os
+
+if os.path.exists("demofile.txt"):
+    os.remove("demofile.txt")
+else:
+    print(" the file does not exists")
+
+
+# delete a folder 
+
+# for delete the entire folder use os.rmdir() 
+
+import os 
+os.rmdir("myfolder")
