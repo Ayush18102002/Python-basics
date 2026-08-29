6c22884d32ef066ae3cb9b5b6d409aaec440a2fa
Ayush <176569369+Ayush18102002@users.noreply.github.com>
2026-08-29 17:36:20 +0530

    pandas introduction

---
+++ pandas/intro.py
@@ -0,0 +1,22 @@
+# pandas introduction
+
+# pandas --->
+
+# padas is a pyhton used for working with data sets
+# it has a functions for analyzing, cleaning, exploring and manupulation data
+# pandas means both "panel data " and "python data analysis".
+
+
+# why use pandas :
+# it allows us to analyze big data and make conclusions based on stastical theories
+# pandas can clean messy data sets  and make them readable and relevants
+# relevant data is very imp. in data science
+
+
+# what can pandas do :
+# it gives you answeres about the data
+# is there a correletion betwn two or more columns.
+# what is average value
+# max , min values
+# python are also able to delete rows that are not revelant 
+
