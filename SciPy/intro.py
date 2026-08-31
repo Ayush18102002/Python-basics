4e7ee6043b391354fa9066cac1555f9e0756b36a
Ayush <176569369+Ayush18102002@users.noreply.github.com>
2026-08-31 08:59:21 +0530

    scipy-introduction

---
+++ SciPy/intro.py
@@ -0,0 +1,15 @@
+# SciPy
+# it is a scientific computation library that uses numpy underneath 
+# it stands for scientific python
+# it provides more utility function for optimization , stats and signall processing 
+# like numpy, scipy is open source so we can use it freely 
+
+
+# why use scipy
+# if scipy uses numpy underneath, why can we not just use numpy?
+# scipy has optimized and added function that are frequently used in nnumpy and data science 
+
+# Which language is scipy written in 
+# SciPy is predominantly written in python, but a few segments are written in c.
+
+
